d15778a1fa6b51e394b4cc58cb31ee108801f1e3
Aaditya135 <66625249+Aaditya135@users.noreply.github.com>
2020-12-30 18:07:08 +0530

    Add files via upload

---
+++ JS/form.js
@@ -0,0 +1,10 @@
+class Form{
+
+    constructor(){
+        this.title = createElement('h1');
+        this.input = createInput('name');
+        this.button = createButton('Play');
+        this.greeting = createElement('h2');
+    }
+
+}
---
+++ sketch.js
@@ -0,0 +1,21 @@
+var database;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var form;
+var player;
+var game;
+
+function setup(){
+
+    createCanvas(500,500);
+
+    database = firebase.database();
+    console.log(database);
+
+}
+
+function draw(){
+   
+}
+
